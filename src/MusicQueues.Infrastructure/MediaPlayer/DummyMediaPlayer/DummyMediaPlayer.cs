d226ed973f87bac152fa8fdc1f32e311482f9134
Blazej Sak <blazej.sak@icloud.com>
2021-09-11 23:15:38 +0200

    Fix dependency issue

--- src/MusicQueues.Infrastructure/MediaPlayer/DummyMediaPlayer/DummyMediaPlayer.cs
+++ src/MusicQueues.Infrastructure/MediaPlayer/DummyMediaPlayer/DummyMediaPlayer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
 using MusicQueues.Application.Common.Interfaces;
 using MusicQueues.Domain.Entities;
 using MusicQueues.Domain.Enums;
@@ -7,41 +8,42 @@ namespace MusicQueues.Infrastructure.MediaPlayer.DummyMediaPlayer
 {
     public class DummyMediaPlayer : IMediaPlayer
     {
-        public DummyMediaPlayer(/*ILogger<DummyMediaPlayer> logger*/)
+        private readonly ILogger<DummyMediaPlayer> _logger;
+            public DummyMediaPlayer(ILogger<DummyMediaPlayer> logger)
         {
-            //_logger = logger;
+            _logger = logger;
         }
 
         public Platform Platform { get; } = Platform.Dummy;
 
         public void QueueCreated(Queue queue)
         {
-            //_logger.LogInformation($"Registered queue {queue.Id}");
+            _logger.LogInformation($"Registered queue {queue.Id}");
         }
 
         public void QueueRemoved(Guid queueId)
         {
-            //_logger.LogInformation($"Unregistered queue {queueId}");
+            _logger.LogInformation($"Unregistered queue {queueId}");
         }
 
         public void SongAdded(Guid queueId, QueueElement element)
         {
-            //_logger.LogInformation($"Added song {element.Id} to queue {queueId}");
+            _logger.LogInformation($"Added song {element.Id} to queue {queueId}");
         }
 
         public void SongMoved(Guid queueId, Guid songId, int position)
         {
-            //_logger.LogInformation($"Moved song {songId} in queue {queueId} into position {position}");
+            _logger.LogInformation($"Moved song {songId} in queue {queueId} into position {position}");
         }
 
         public void SongRemoved(Guid queueId, Guid songId)
         {
-            //_logger.LogInformation($"Removed song {songId} from {queueId}");
+            _logger.LogInformation($"Removed song {songId} from {queueId}");
         }
 
         public void RefreshQueue(Queue queue)
         {
-            //_logger.LogInformation($"Refreshed queue {queue.Id}");
+            _logger.LogInformation($"Refreshed queue {queue.Id}");
         }
     }
 }
