fd8adc8a45a64047a9a7706f6bdd9d272d2f823c
Blazej Sak <blazej.sak@icloud.com>
2022-07-15 16:40:26 +0200

    Add seed method for Spotify in DebugController.cs

--- src/MusicQueues.Api/Controllers/DebugController.cs
+++ src/MusicQueues.Api/Controllers/DebugController.cs
@@ -19,8 +19,8 @@ namespace MusicQueues.Api.Controllers
             _mediator = mediator;
         }
 
-        [HttpGet("init-db")]
-        public async Task<Guid> InitDb()
+        [HttpGet("init-dummy-db")]
+        public async Task<Guid> InitDummyDb()
         {
             var qId = await _mediator.Send(new CreateQueue(Platform.Dummy, "Sample MusicQueue", "MusicQueues is up!"));
             await _mediator.Send(new AddElement(qId, "rickroll-01", "Never Gonna Give You Up"));
@@ -30,6 +30,19 @@ namespace MusicQueues.Api.Controllers
             await _mediator.Send(new AddElement(qId, "allout80s/4", "I Just Died in Your Arms"));
             await _mediator.Send(new AddElement(qId, "allout80s/5", "Toto Africa"));
             return qId;
-        } 
+        }
+
+        [HttpGet("init-spotify-db")]
+        public async Task<Guid> InitSpotifyDb()
+        {
+            var qId = await _mediator.Send(new CreateQueue(Platform.Spotify, "Sample MusicQueue for Spotify", "MusicQueues is up! Remember to connect your Spotify account via /queue/{id}/setup endpoint"));
+            await _mediator.Send(new AddElement(qId, "spotify:track:4cOdK2wGLETKBW3PvgPWqT", "Never Gonna Give You Up"));
+            await _mediator.Send(new AddElement(qId, "spotify:track:2WfaOiMkCvy7F5fcp2zZ8L", "Take on Me"));
+            await _mediator.Send(new AddElement(qId, "spotify:track:5ChkMS8OtdzJeqyybCc9R5", "Billie Jean"));
+            await _mediator.Send(new AddElement(qId, "spotify:track:1OOtq8tRnDM8kG2gqUPjAj", "Beat It"));
+            await _mediator.Send(new AddElement(qId, "spotify:track:4ByEFOBuLXpCqvO1kw8Wdm", "I Just Died in Your Arms"));
+            await _mediator.Send(new AddElement(qId, "spotify:track:2374M0fQpWi3dLnB54qaLX", "Toto Africa"));
+            return qId;
+        }
     }
 }
